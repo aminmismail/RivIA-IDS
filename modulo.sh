e950944d40e9f22711934a90def67d3c74ce2414
Amin Ismail <44300857+aminmismail@users.noreply.github.com>
2024-02-07 17:40:09 -0300

    Added ; to if

--- modulo.sh
+++ modulo.sh
@@ -12,11 +12,11 @@ echo
 echo -n ">"
 read opt
 
-if [[ $opt -eq 1 ]] then
+if [[ $opt -eq 1 ]]; then
   echo ">Digite a interface"
    read interface
    ./capture_interface_pcap.sh $interface pcap $(id -nu 1000)
-elif [[ $opt -eq 2 ]] then
+elif [[ $opt -eq 2 ]]; then
    echo ">Digite o nome do arquivo PCAP"
    read pcap_file
    ./convert_pcap_csv.sh $pcap_file
