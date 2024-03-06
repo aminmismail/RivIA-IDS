bd7119c167c3f890d04d5dda6bd43fc243326441
Amin Ismail <44300857+aminmismail@users.noreply.github.com>
2024-03-06 19:11:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 pip3 install -r requirements.txt && \
 sudo apt install libpcap-dev && \
-sudo apt install default-jre
-#Faltando chmod dos arquivos (a verificar ainda)
+sudo apt install default-jre && \
+chmod +x modulo.sh && \
+chmod +x CICFlowMeters/CICFlowMeter-4.0/bin/CICFlowMeter
+chmod 700 convert_pcap_csv.sh && \
+chmod 700 capture_interface_pcap.sh
